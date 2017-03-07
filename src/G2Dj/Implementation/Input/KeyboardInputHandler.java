a5cdb904a82a3cea5bef0d8795dc07cd68194dae
Joseph Cameron <jf.cameron@gmail.com>
2017-03-07 07:16:08 -0500

    inital commit

---
+++ src/G2Dj/Implementation/Input/KeyboardInputHandler.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package G2Dj.Implementation.Input;
+
+/**
+ *
+ * @author Joe
+ */
+public class KeyboardInputHandler 
+{
+    
+    
+    public boolean getKeyDown()
+    {
+        //TODO: implementation
+        
+        return false;
+        
+    }
+    
+}
