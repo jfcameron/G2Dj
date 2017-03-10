d9b1f6ec79c6d9bc0579f8ad8be169d06df58c73
Joseph Cameron <jf.cameron@gmail.com>
2017-03-09 23:41:40 -0500

    Complete Camera 1st iteration, beginning Shader & ShaderCollection class dec&defs
    Changed camera to store screen pos/size as nsc rather than pixel, since
    screen may change arbitrarily & therefore pixel values will have to be
    recalcualted before passing to gl anyway.
    Beginning work on ShaderCollection and Shader objects.

--- src/G2Dj/Graphics.java
+++ src/G2Dj/Graphics.java
@@ -31,9 +31,11 @@ public class Graphics
     
     //getters
     public static Window getWindow(){return s_Window;}
+    public static GL     getGL    (){return s_GL;    }
     
     protected static void init(){}
     
+    //TEST AREA
     private static Camera camera1 = new Camera(new Vector2(0.0f,0.0f), new Vector2(0.5f,0.5f),Color.CornflowerBlue(),CameraClearMode.Color);
     private static Camera camera2 = new Camera(new Vector2(0.0f,0.5f), new Vector2(0.5f,0.5f),Color.DeathlyPink(),CameraClearMode.Color);
     private static Camera camera3 = new Camera(new Vector2(0.5f,0.0f), new Vector2(0.5f,0.5f),Color.Red(),CameraClearMode.Color);
@@ -41,48 +43,13 @@ public class Graphics
     
     protected static void draw()
     {
+        //TEST AREA
         camera1.draw(s_GL);
         camera2.draw(s_GL);
         camera3.draw(s_GL);
         camera4.draw(s_GL);
-        
-        /*
-        GL gl = s_GL;
-        
-        //Camera1
-        {
-            //c1 init()
-            gl.glEnable(GL.GL_DEPTH_TEST);
-            gl.glEnable(GL.GL_SCISSOR_TEST);
-            
-            int vpX2 = s_Window.getWidth(), vpY2 = s_Window.getHeight(),
-                vpX1 = 0, vpY1 = 0;
-            
-            //c1 draw()
-            gl.glViewport(vpX1,vpY1,vpX2,vpY2);
-            gl.glScissor (vpX1,vpY1,vpX2,vpY2);
-            
-            gl.glDepthMask(true);
-            
-            //gl.glClearColor(1.0f, 0.5f, 0.0f, 1.0f);
-            gl.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT);
-            
-        }*/
-
-        /*
-        ////////TEST JUNK////////////
-        if (Input.getKey(KeyCode.A))
-        {
-            gl.glClearColor(0.0f, 0.5f, 0.0f, 1.0f);
-            
-        }
-        
-        if (Input.getKey(KeyCode.S))
-        {
-            gl.glClearColor(0.0f, 0.1f, 0.7f, 1.0f);
-            
-        }
-        */
+                
+        //update canvas, swap buffers
         s_Window.draw();
         
     }
--- src/G2Dj/Imp/Graphics/Camera.java
+++ src/G2Dj/Imp/Graphics/Camera.java
@@ -5,6 +5,7 @@
  */
 package G2Dj.Imp.Graphics;
 
+import G2Dj.Debug;
 import G2Dj.Graphics;
 import G2Dj.Math.IntVector2;
 import G2Dj.Math.Vector2;
@@ -22,37 +23,37 @@ public class Camera
     //*************
     private Color           m_ClearColor;		
     private CameraClearMode m_ClearMode;
-    private IntVector2      m_ViewportPosition;
-    private IntVector2      m_ViewportSize;
+    private Vector2         m_ViewportPosition;
+    private Vector2         m_ViewportSize;
     //private RenderTexture m_RenderTexture;
     
     //**********
     // Accessors
     //**********
-    public Color           getClearColor      (){return m_ClearColor;      }
-    public CameraClearMode getClearMode       (){return m_ClearMode;       }
-    public IntVector2      getViewportPixelPosition(){return m_ViewportPosition;}
-    public IntVector2      getViewportPixelSize    (){return m_ViewportSize;    }
+    public Color           getClearColor            (){return m_ClearColor;      }
+    public CameraClearMode getClearMode             (){return m_ClearMode;       }
+    public Vector2         getViewportScreenPosition(){return m_ViewportPosition;}
+    public Vector2         getViewportScreenSize    (){return m_ViewportSize;    }
     //Math::Vector3 getWorldPointFromScreenPoint(const Math::Vector2 &aScreenPoint);
     //Math::Vector3 getWorldPointFromScreenPoint(const Math::Vector2 &aScreenPoint, const float &aWorldSpaceDistance);
     
-    public Vector2 getViewportScreenPosition()
+    public IntVector2 getViewportPixelPosition()
     {
-        return new Vector2
+        return new IntVector2
         (
-            m_ViewportPosition.x * Graphics.getWindow().getWidth(),
-            m_ViewportPosition.y * Graphics.getWindow().getHeight()
+            (int)(m_ViewportPosition.x * Graphics.getWindow().getWidth ()),
+            (int)(m_ViewportPosition.y * Graphics.getWindow().getHeight())
                 
         );
         
     }
     
-    public Vector2 getViewportScreenSize()
+    public IntVector2 getViewportPixelSize()
     {
-        return new Vector2
+        return new IntVector2
         (
-            m_ViewportSize.x * Graphics.getWindow().getWidth(),
-            m_ViewportSize.y * Graphics.getWindow().getHeight()
+            (int)(m_ViewportSize.x * Graphics.getWindow().getWidth ()),
+            (int)(m_ViewportSize.y * Graphics.getWindow().getHeight ())
                 
         );
         
@@ -60,17 +61,16 @@ public class Camera
     
     public void setClearColor           (final Color           aClearColor      ){m_ClearColor       = aClearColor;      }
     public void setClearMode            (final CameraClearMode aClearMode       ){m_ClearMode        = aClearMode;       }
-    public void setViewportPixelPosition(final IntVector2      aViewportPosition){m_ViewportPosition = aViewportPosition;}
-    public void setViewportPixelSize    (final IntVector2      aViewportSize    ){m_ViewportSize     = aViewportSize;    }
+    //public void setViewportPixelPosition(final IntVector2      aViewportPosition){m_ViewportPosition = aViewportPosition;}
+    //public void setViewportPixelSize    (final IntVector2      aViewportSize    ){m_ViewportSize     = aViewportSize;    }
     
     public void draw(GL aGL) //void draw(void) override;
-    {
-        //This should be moved to an init
-        aGL.glEnable(GL.GL_DEPTH_TEST);
-        aGL.glEnable(GL.GL_SCISSOR_TEST);
-                                
-        aGL.glViewport(m_ViewportPosition.x,m_ViewportPosition.y,m_ViewportSize.x,m_ViewportSize.y);
-        aGL.glScissor (m_ViewportPosition.x,m_ViewportPosition.y,m_ViewportSize.x,m_ViewportSize.y);
+    {   
+        //Update viewport
+        IntVector2 pixPos = getViewportPixelPosition(), pixSize = getViewportPixelSize();
+        //Debug.log(pixSize);
+        aGL.glViewport(pixPos.x,pixPos.y,pixSize.x,pixSize.y);
+        aGL.glScissor (pixPos.x,pixPos.y,pixSize.x,pixSize.y);
           
         aGL.glDepthMask(true);  
         
@@ -99,11 +99,15 @@ public class Camera
     }
     public Camera(Vector2 aViewportScreenPosition, Vector2 aViewportScreenSize, Color aClearColor, CameraClearMode aClearMode)
     {
-        m_ClearColor       = aClearColor      ;		
-        m_ClearMode        = aClearMode       ;
+        m_ClearColor       = aClearColor;		
+        m_ClearMode        = aClearMode;
+        m_ViewportPosition = aViewportScreenPosition;
+        m_ViewportSize     = aViewportScreenSize;
         
-        m_ViewportPosition = new IntVector2((int)(aViewportScreenPosition.x*Graphics.getWindow().getWidth()),(int)(aViewportScreenPosition.y*Graphics.getWindow().getHeight()));
-        m_ViewportSize     = new IntVector2((int)(aViewportScreenSize.x    *Graphics.getWindow().getWidth()),(int)(aViewportScreenSize.y    *Graphics.getWindow().getHeight()));
+        //GL init
+        GL gl = Graphics.getGL();
+        gl.glEnable(GL.GL_DEPTH_TEST);
+        gl.glEnable(GL.GL_SCISSOR_TEST);
         
     }
     
