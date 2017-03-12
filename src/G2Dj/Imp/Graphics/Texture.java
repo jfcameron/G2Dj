f547854b440567c314820dabc666bea0c8bce63b
Joseph Cameron <jf.cameron@gmail.com>
2017-03-12 16:00:09 -0400

    Fixed texture format issue
    The closest Java's ImageIO gets to RGBA is BGRA. Wrote a function to
    convert to RGBA.

--- src/G2Dj/Imp/Graphics/Texture.java
+++ src/G2Dj/Imp/Graphics/Texture.java
@@ -8,6 +8,9 @@ package G2Dj.Imp.Graphics;
 import G2Dj.Debug;
 import G2Dj.Graphics;
 import com.jogamp.opengl.GL;
+import static com.jogamp.opengl.GLProfile.GL2;
+import static com.jogamp.opengl.GLProfile.GL3;
+import static com.jogamp.opengl.GLProfile.GL4;
 import com.sun.prism.impl.BufferUtil;
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferByte;
@@ -61,7 +64,14 @@ public class Texture extends GraphicsObject
             BufferedImage img = null;// new BufferedImage(w, h, BufferedImage.YTPE_4BYTE_ABGR);
             try 
             {
-                img = ImageIO.read(new File("brick.png"));
+                /*BufferedImage raw*/ img = ImageIO.read(new File("brick.png"));
+                //img = new BufferedImage(raw.getWidth(),raw.getHeight(),BufferedImage.)
+                
+                //BufferedImage img= new BufferedImage(src.getWidth(), src.getHeight(), bufImgType);
+                //Graphics2D g2d= img.createGraphics();
+                //g2d.drawImage(src, 0, 0, null);
+                //g2d.dispose();
+                //return img;
                 
             }
             catch (IOException ex) 
@@ -73,23 +83,35 @@ public class Texture extends GraphicsObject
             Debug.log("Dimensons: {"+img.getWidth()+", "+img.getWidth()+"}");
 
             int[] data = img.getRGB(0, 0, img.getWidth(), img.getHeight(), (int[])null, 0, img.getWidth());
+            
+            //Change from BGRA to RGBA
+            for(int i = 0, pixel,r,g,b,a; i < data.length; i++)
+            {             
+                pixel = data[i];
+                a = (pixel >> 24) & 0xff;
+                r = (pixel >> 16) & 0xff;
+                g = (pixel >> 8) & 0xff;
+                b = (pixel >> 0) & 0xff;
+                pixel = (b<<16) | (g<<8) | (r<<0) | (a<<24);
+                data[i] = pixel;
                 
-            ////////////////SET IT WHITE
-            //for(int i = 0; i < data.length; i++)
-            //    data[i] = 2147483647;
+             
+            }
+            
+           // Debug.log(Integer.toBinaryString(data[0]));
                 
             ////////////////////////////
                 
             IntBuffer pngbuffer = IntBuffer.wrap(data);
                 
-            int pixel = pngbuffer.get(0);
+            //int pixel = pngbuffer.get(0);
                 
-            int alpha = (pixel >> 24) & 0xff;
+            /*int alpha = (pixel >> 24) & 0xff;
             int red = (pixel >> 16) & 0xff;
             int green = (pixel >> 8) & 0xff;
-            int blue = (pixel >> 0) & 0xff;
+            int blue = (pixel >> 0) & 0xff;*/
             
-            Debug.log("THIS IS THE PIXEL DATA: "+red,green,blue,alpha);
+            //Debug.log("THIS IS THE PIXEL DATA: "+red,green,blue,alpha);
             
             
             //Debug.log("Byte array: "+pngbuffer.array().length);
@@ -103,30 +125,20 @@ public class Texture extends GraphicsObject
             IntBuffer texturehandle = IntBuffer.allocate(1);//.get(0);
             //int[] texturehandle = new int[1];
                 
-                
+            int textureFormat = GL.GL_RGBA;
+            
             //Put the texture data in video memory
             gl.glGenTextures( 1, texturehandle );
             gl.glActiveTexture( GL.GL_TEXTURE0 );
             gl.glBindTexture( GL.GL_TEXTURE_2D, texturehandle.get(0) );
-            gl.glTexImage2D( GL.GL_TEXTURE_2D, 0, GL.GL_RGBA, img.getWidth(), img.getHeight(), 0, GL.GL_RGBA, GL.GL_UNSIGNED_BYTE, pngbuffer );
-		
+            gl.glTexImage2D( GL.GL_TEXTURE_2D, 0, textureFormat, img.getWidth(), img.getHeight(), 0, textureFormat, GL.GL_UNSIGNED_BYTE, pngbuffer );
             
-
             //Apply parameters
-            int repeatmode = 0;
-            int magfilter = 0;
-            
-            gl.glTexParameteri( GL.GL_TEXTURE_2D, GL.GL_TEXTURE_WRAP_S, repeatmode);
-            gl.glTexParameteri( GL.GL_TEXTURE_2D, GL.GL_TEXTURE_WRAP_T, repeatmode);
-	
-            gl.glTexParameteri(GL.GL_TEXTURE_2D, GL.GL_TEXTURE_MAG_FILTER, magfilter );
-            gl.glTexParameteri(GL.GL_TEXTURE_2D, GL.GL_TEXTURE_MIN_FILTER, GL.GL_LINEAR );
-
-            gl.glTexParameterf(GL.GL_TEXTURE_2D, GL.GL_TEXTURE_MAG_FILTER, GL.GL_NEAREST);
+            gl.glTexParameteri(GL.GL_TEXTURE_2D, GL.GL_TEXTURE_MAG_FILTER, GL.GL_NEAREST );
+            gl.glTexParameteri(GL.GL_TEXTURE_2D, GL.GL_TEXTURE_MIN_FILTER, GL.GL_NEAREST );
 		    
-            Debug.log("Error: "+gl.glGetError());
+            //Debug.log("Error: "+gl.glGetError());
             
-            //m_Name = filename;
             m_TextureHandle = texturehandle.get(0);
                 
         }
