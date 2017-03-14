abcc6fdd3f40921a9abd69793b28eaf1cd18732c
Joseph Cameron <jf.cameron@gmail.com>
2017-03-14 17:58:45 -0400

    Working on transformations & math
    Quaternion stub added to Math package. Transform being implemented.
    Cleaned LOG.MD

--- LOG.md
+++ LOG.md
@@ -1,5 +1,4 @@
-Java generics done via erasure. cannot do such things as T t new T() or something instanceof T. etc.
-GameObject.addComponent(T) works however
-GameObject.removeComponent(T) does not supply enough info to find T in the component vector
-1 solution: hashmap <String,Component> and removeComponent(final String aComponentType).
-Not elegant but allows me to continue to not allow user to instantiate class types owned by the engine outside of the engine.
+
+
+
+throw new java.lang.UnsupportedOperationException("Not supported yet.");
---
+++ src/G2Dj/Imp/Engine/Transform.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package G2Dj.Imp.Engine;
+
+import G2Dj.Math.Quaternion;
+import G2Dj.Math.Vector3;
+
+/**
+ *
+ * @author Joe
+ */
+public class Transform 
+{
+    //
+    //
+    //
+    private Vector3    m_Position, m_Scale;
+    private Quaternion m_Rotation;
+    
+    //
+    //
+    //
+    public Vector3    getPosition(){return m_Position;}
+    public Vector3    getScale   (){return m_Scale;   }
+    public Quaternion getRotation(){return m_Rotation;}
+    public Vector3    getEulers  (){throw new java.lang.UnsupportedOperationException("Not supported yet.");}
+    
+    public void setPosition(final Vector3    aPosition){m_Position = aPosition;}
+    public void setScale   (final Vector3    aScale   ){m_Scale    = aScale;   }
+    public void setRotation(final Vector3    aRotation){throw new java.lang.UnsupportedOperationException("Not supported yet.");}
+    public void setRotation(final Quaternion aRotation){throw new java.lang.UnsupportedOperationException("Not supported yet.");}
+    //
+    //
+    //    
+    public void translate(final Vector3    aTranslation){m_Position.addInPlace(aTranslation);}
+    public void scale    (final Vector3    aScale      ){throw new java.lang.UnsupportedOperationException("Not supported yet.");}
+    public void scale    (final float      aScalar     ){m_Scale.multiply(aScalar);}
+    public void rotate   (final Vector3    aRotation   ){throw new java.lang.UnsupportedOperationException("Not supported yet.");}
+    public void rotate   (final Quaternion aRotation   ){throw new java.lang.UnsupportedOperationException("Not supported yet.");}
+    
+}
