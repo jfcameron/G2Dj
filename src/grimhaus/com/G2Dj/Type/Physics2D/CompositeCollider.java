f41e86173a49636bebfec683d80c8882e97f8bcd
Joseph Cameron <jf.cameron@gmail.com>
2017-03-24 02:40:06 -0400

    working on composite collider

--- src/grimhaus/com/G2Dj/Type/Physics2D/CompositeCollider.java
+++ src/grimhaus/com/G2Dj/Type/Physics2D/CompositeCollider.java
@@ -7,7 +7,6 @@ package grimhaus.com.G2Dj.Type.Physics2D;
 import grimhaus.com.G2Dj.Imp.Physics2D.Collider;
 import grimhaus.com.G2Dj.Type.Graphics.LineVisualizer;
 import org.jbox2d.collision.shapes.PolygonShape;
-import org.jbox2d.collision.shapes.Shape;
 import org.jbox2d.dynamics.FixtureDef;
 
 /**
@@ -18,7 +17,6 @@ public class CompositeCollider extends Collider
 {
     protected FixtureDef[]     m_FixtureDefinition;
     protected LineVisualizer[] m_LineVisualizer;    
-    protected PolygonShape[]   m_Shape;
     
     @Override
     public FixtureDef[] getB2DFixtures()
@@ -33,17 +31,23 @@ public class CompositeCollider extends Collider
     @Override
     protected void buildShape() 
     {
-        ///TEST AREA
-        //m_Shape = new PolygonShape[]{};
-        //m_Shape[0].set
+        //TEST AREA
+        int len = 3;
+        m_FixtureDefinition = new FixtureDef    [len];
+        m_LineVisualizer    = new LineVisualizer[len]; 
         
-        ///ENDOFTSETAREA
-        if (m_FixtureDefinition==null || m_Shape==null || m_LineVisualizer==null)
-            return;
-        
-        for(int i=0,s=m_FixtureDefinition.length;i<s;i++)
-            if (m_Shape[i] != null)
-                m_FixtureDefinition[i].setShape(m_Shape[i]);
+        for(int i=0,s=len;i<s;i++)
+        {
+            //m_LineVisualizer[i] = (LineVisualizer)getGameObject().get().addComponent(LineVisualizer.class);
+            
+            m_FixtureDefinition[i] = new FixtureDef();
+            
+            m_FixtureDefinition[i].setShape(new PolygonShape());
+            
+            
+            m_FixtureDefinition[i].density = 1;
+            
+        }
         
     }
     
