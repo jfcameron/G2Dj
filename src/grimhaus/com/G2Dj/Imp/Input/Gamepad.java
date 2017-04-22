0ce6d7759a01b8441fb3beb858aacf7de8b33988
Joseph Cameron <jf.cameron@gmail.com>
2017-04-22 05:11:07 -0400

    continuing work on gamepad input handling. added stubs for Button, Axis, Hat

--- src/grimhaus/com/G2Dj/Imp/Input/Gamepad.java
+++ src/grimhaus/com/G2Dj/Imp/Input/Gamepad.java
@@ -6,6 +6,8 @@ package grimhaus.com.G2Dj.Imp.Input;
 
 import grimhaus.com.G2Dj.Debug;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import net.java.games.input.Component;
 import net.java.games.input.Component.Identifier;
 import net.java.games.input.Controller;
@@ -15,17 +17,57 @@ import net.java.games.input.Controller;
  * @author Joseph Cameron
  */
 public class Gamepad 
-{
+{    
     //
     //
     //
-  //  private final Component[] m_Axes;
-  //  private final Component[] m_Hats;
-  //  private final Component[] m_Buttons;
+    private final String m_Name;
+    
+    private final HashMap<String,Component> m_Axes    = new HashMap<>();
+    private final HashMap<String,Component> m_Hats    = new HashMap<>();
+    private final HashMap<String,Component> m_Buttons = new HashMap<>();
     
     //
     //
     //
+    public String getName(){return m_Name;}
+    
+    public final String[] getAxisNames()
+    {
+        String[] rvalue = new String[m_Axes.size()];
+        
+        int i =0;
+        for(Map.Entry<String,Component> entry : m_Axes.entrySet())
+            rvalue[i++] = entry.getKey();
+        
+        return rvalue;
+        
+    }
+    
+    public final String[] getHatNames()
+    {
+        String[] rvalue = new String[m_Hats.size()];
+        
+        int i =0;
+        for(Map.Entry<String,Component> entry : m_Hats.entrySet())
+            rvalue[i++] = entry.getKey();
+        
+        return rvalue;
+        
+    }
+    
+    public final String[] getButtonNames()
+    {
+        String[] rvalue = new String[m_Buttons.size()];
+        
+        int i =0;
+        for(Map.Entry<String,Component> entry : m_Buttons.entrySet())
+            rvalue[i++] = entry.getKey();
+        
+        return rvalue;
+        
+    }
+    
     public boolean getButton(final String aButtonName)
     {
         throw new java.lang.UnsupportedOperationException("Not supported yet.");
@@ -38,28 +80,61 @@ public class Gamepad
         
     }
     
+    public float getAxis(final String aAxisName)
+    {
+        throw new java.lang.UnsupportedOperationException("Not supported yet.");
+        
+    }
+    
+    //public HatEnum getHat(){}
+    
     //
     //
     //
     public Gamepad(final net.java.games.input.Controller aJInputControllerHandle)
     {
-        Component[] components = aJInputControllerHandle.getComponents();
-        ArrayList<Component> axes    = new ArrayList<>();
-        ArrayList<Component> hats    = new ArrayList<>();
-        ArrayList<Component> buttons = new ArrayList<>();
+        m_Name = aJInputControllerHandle.getName();
         
-        Debug.log("**********Gamepad******************");
+        Component[] components = aJInputControllerHandle.getComponents();        
+        Component currentComponent;
         
         for(int i=0,s=components.length;i<s;i++)
-            if (components[i].getIdentifier() instanceof Identifier.Button)
-                buttons.add(components[i]);
-            else if (components[i].getIdentifier() == Identifier.Axis.POV)
-            {
-                Debug.log("SUCCESS @ "+components[i].getName());
-                
-            }   
-                
+        {
+            currentComponent = components[i];
             
+            if (currentComponent.getIdentifier() instanceof Identifier.Button)
+                m_Buttons.put(currentComponent.getName(),currentComponent);
+            else if (currentComponent.getIdentifier() instanceof Identifier.Axis)
+            {
+                if (currentComponent.getIdentifier() == Identifier.Axis.POV)
+                    m_Hats.put(currentComponent.getName(),currentComponent);
+                else
+                    m_Axes.put(currentComponent.getName(),currentComponent);
+                                
+            }
+                
+        }
+        
+    }
+    
+    //
+    // inner classes
+    //
+    private class Button
+    {
+        
+        
+    }
+    
+    private class Axis
+    {
+        
+        
+    }
+    
+    private class Hat
+    {
+        
         
     }
     
