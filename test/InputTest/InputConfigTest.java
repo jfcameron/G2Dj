92d58ed10705899a8feca7b792265bc503285ec6
Joseph Cameron <jf.cameron@gmail.com>
2017-04-26 20:51:07 -0400

    updated inputtest to demonstrate multiple gamepads

--- test/InputTest/InputConfigTest.java
+++ test/InputTest/InputConfigTest.java
@@ -20,7 +20,8 @@ import java.lang.ref.WeakReference;
  */
 public class InputConfigTest extends Component 
 {
-    private InputConfig m_PlayerControls;
+    private InputConfig m_Player1Controls;
+    private InputConfig m_Player2Controls;
     
     private final String s_Up    = "Up";
     private final String s_Down  = "Down";
@@ -31,7 +32,7 @@ public class InputConfigTest extends Component
     @Override
     protected void initialize() 
     { 
-        m_PlayerControls = new InputConfig
+        m_Player1Controls = new InputConfig
         (
             new InputEvent(s_Up   ),
             new InputEvent(s_Down ),
@@ -40,28 +41,33 @@ public class InputConfigTest extends Component
                 
         );
         
-        m_PlayerControls.addKeysToAEvent(s_Up, KeyCode.W);
-        m_PlayerControls.addGamepadButtonsToAEvent(s_Up,"Button 1");
-        m_PlayerControls.addGamepadHatToAEvent(s_Up,"Hat Switch",Hat.Direction.Up);
+        m_Player1Controls.addKeysToAEvent(s_Up, KeyCode.W);
+        m_Player1Controls.addGamepadButtonsToAEvent(s_Up,"Button 1");
+        m_Player1Controls.addGamepadHatToAEvent(s_Up,"Hat Switch",Hat.Direction.Up);
+        
+        m_Player2Controls = new InputConfig
+        (
+            new InputEvent(s_Up   ),
+            new InputEvent(s_Down ),
+            new InputEvent(s_Left ),
+            new InputEvent(s_Right)
+                
+        );
+        
+        m_Player2Controls.addKeysToAEvent(s_Up, KeyCode.I);
+        m_Player2Controls.addGamepadButtonsToAEvent(s_Up,"Button 1");
+        m_Player2Controls.addGamepadHatToAEvent(s_Up,"Hat Switch",Hat.Direction.Up);
                 
     }
 
     @Override
     protected void update() 
     {
-        if (m_PlayerControls.getDown(s_Up))
-            Debug.log(s_Up);
+        if (m_Player1Controls.getDown(s_Up))
+            Debug.log("P1: "+s_Up);
         
-        if (m_PlayerControls.getDown(s_Down))
-            Debug.log(s_Down);
-        
-        if (m_PlayerControls.getDown(s_Left))
-            Debug.log(s_Left);
-        
-        if (m_PlayerControls.getDown(s_Right))
-            Debug.log(s_Right);
-        
-        
+        if (m_Player2Controls.getDown(s_Up))
+            Debug.log("P2: "+s_Up);
         
     }
 
